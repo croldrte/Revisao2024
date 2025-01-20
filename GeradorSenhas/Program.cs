00af991ffe0bc5e1e0192979fd4fee9b7ee2693f
Carol Duarte <carolduarte2000@gmail.com>
2025-01-20 17:59:35 -0300

    Configura preferências de senha com validações de entrada

--- GeradorSenhas/Program.cs
+++ GeradorSenhas/Program.cs
@@ -1,1 +1,35 @@
-﻿
+﻿Console.Clear();
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("=".PadLeft(17, '='));
+Console.WriteLine("Gerador de Senhas");
+Console.WriteLine("=".PadLeft(17, '='));
+Console.ResetColor();
+
+Console.Write("Sua senha deve conter quantos caracteres?: ");
+int qtdCaracteres;
+while (!int.TryParse(Console.ReadLine(), out qtdCaracteres) || qtdCaracteres <= 0)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.Write("Resposta inválida. Por favor, insira um número inteiro maior que 0: ");
+    Console.ResetColor();
+}
+
+Console.Write("Sua senha deve conter letras? (S/N): ");
+string temLetras = Console.ReadLine().ToUpper();
+while (temLetras != "S" && temLetras != "N")
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.Write("Resposta inválida. Por favor, insira 'S' para Sim e 'N' para Não: ");
+    Console.ResetColor();
+    temLetras = Console.ReadLine().ToUpper();
+}
+
+Console.Write("Sua senha deve conter caracteres especiais? (S/N): ");
+string temEspeciais = Console.ReadLine().ToUpper();
+while (temEspeciais != "S" && temEspeciais != "N")
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.Write("Resposta inválida. Por favor, insira 'S' para Sim e 'N' para Não: ");
+    Console.ResetColor();
+    temEspeciais = Console.ReadLine().ToUpper();
+}
