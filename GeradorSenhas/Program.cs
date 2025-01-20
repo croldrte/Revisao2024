69a5fde7e45f5c32e9372da59ab624f358362dd5
Carol Duarte <carolduarte2000@gmail.com>
2025-01-20 18:08:29 -0300

    Geração de letras aleatórias com base nas preferências

--- GeradorSenhas/Program.cs
+++ GeradorSenhas/Program.cs
@@ -1,4 +1,8 @@
-﻿Console.Clear();
+﻿char[] numeros = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+char[] letras = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+char[] especiais = { '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '{', '}', '[', ']', '/', '?', '"', ':', ';' };
+
+Console.Clear();
 Console.ForegroundColor = ConsoleColor.Green;
 Console.WriteLine("=".PadLeft(17, '='));
 Console.WriteLine("Gerador de Senhas");
@@ -32,4 +36,47 @@ while (temEspeciais != "S" && temEspeciais != "N")
     Console.Write("Resposta inválida. Por favor, insira 'S' para Sim e 'N' para Não: ");
     Console.ResetColor();
     temEspeciais = Console.ReadLine().ToUpper();
+}
+
+double qtdNumeros = 0, qtdLetras, qtdEspeciais;
+char[] numerosSenha = new char[qtdCaracteres - 1];
+Random aleatorio = new Random();
+
+if (temLetras == "S" && temEspeciais == "S")
+{
+    qtdNumeros = Math.Round(qtdCaracteres * .5, 0);
+    qtdLetras = Math.Round(qtdCaracteres * .3, 0);
+    qtdEspeciais = Math.Round(qtdCaracteres * .2, 0);
+}
+else if (temLetras == "S" && temEspeciais == "N")
+{
+    qtdNumeros = Math.Round(qtdCaracteres * .6, 0);
+    qtdLetras = Math.Round(qtdCaracteres * .4, 0);
+    qtdEspeciais = 0;
+}
+else if (temLetras == "N" && temEspeciais == "S")
+{
+    qtdNumeros = Math.Round(qtdCaracteres * .7, 0);
+    qtdLetras = 0;
+    qtdEspeciais = Math.Round(qtdCaracteres * .3, 0);
+}
+else
+{
+    qtdNumeros = qtdCaracteres;
+    qtdLetras = 0;
+    qtdEspeciais = 0;
+}
+
+for (int n = 0; n <= qtdNumeros; n++)
+{  
+    for (int s = 0; s <= qtdNumeros; s++)
+    { 
+        int numeroAleatorio = aleatorio.Next(numeros.Length);
+        numerosSenha[s] = letras[numeroAleatorio];       
+    }
+}   
+
+foreach (char n in numerosSenha)
+{
+    Console.WriteLine(n);
 }
