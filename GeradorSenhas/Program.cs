edb8b4b87a49ac633deb21d4b65af73d8dda7f15
Carol Duarte <carolduarte2000@gmail.com>
2025-01-20 19:19:45 -0300

    Gera senha aleatória com base nas preferências do usuário

--- GeradorSenhas/Program.cs
+++ GeradorSenhas/Program.cs
@@ -1,5 +1,6 @@
 ﻿char[] numeros = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-char[] letras = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+char[] letrasMi = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+char[] letrasMa = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
 char[] especiais = { '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '{', '}', '[', ']', '/', '?', '"', ':', ';' };
 
 Console.Clear();
@@ -38,64 +39,65 @@ while (temEspeciais != "S" && temEspeciais != "N")
     temEspeciais = Console.ReadLine().ToUpper();
 }
 
-int qtdNumeros, qtdLetras, qtdEspeciais;
+int qtdNumeros, qtdLetrasMi, qtdLetrasMa, qtdEspeciais;
 Random aleatorio = new Random();
 
 if (temLetras == "S" && temEspeciais == "S")
 {
     qtdNumeros = (int)Math.Round(qtdCaracteres * .5, 0);
-    qtdLetras = (int)Math.Round(qtdCaracteres * .3, 0);
+    qtdLetrasMi = qtdLetrasMa = (int)Math.Round(qtdCaracteres * .15, 0);
     qtdEspeciais = (int)Math.Round(qtdCaracteres * .2, 0);
 }
 else if (temLetras == "S" && temEspeciais == "N")
 {
     qtdNumeros = (int)Math.Round(qtdCaracteres * .6, 0);
-    qtdLetras = (int)Math.Round(qtdCaracteres * .4, 0);
+    qtdLetrasMi = qtdLetrasMa = (int)Math.Round(qtdCaracteres * .2, 0);
     qtdEspeciais = 0;
 }
 else if (temLetras == "N" && temEspeciais == "S")
 {
     qtdNumeros = (int)Math.Round(qtdCaracteres * .7, 0);
-    qtdLetras = 0;
+    qtdLetrasMi = qtdLetrasMa = 0;
     qtdEspeciais = (int)Math.Round(qtdCaracteres * .3, 0);
 }
 else
 {
     qtdNumeros = qtdCaracteres;
-    qtdLetras = 0;
-    qtdEspeciais = 0;
+    qtdLetrasMi = qtdLetrasMa = qtdEspeciais = 0;
 }
 
 char[] numerosSenha = new char[qtdNumeros];
-char[] letrasSenha = new char[qtdLetras];
+char[] letrasMiSenha = new char[qtdLetrasMi];
+char[] letrasMaSenha = new char[qtdLetrasMa];
 char[] especiaisSenha = new char[qtdEspeciais];
 
 for (int q = 0; q <= qtdNumeros - 1; q++)
 {
-    int numeroAleatorio = aleatorio.Next(numeros.Length - 1);
-    numerosSenha[q] = numeros[numeroAleatorio];
+    int caractereAleatorio = aleatorio.Next(numeros.Length);
+    numerosSenha[q] = numeros[caractereAleatorio];
 }
-foreach (char n in numerosSenha)
+for (int q = 0; q <= qtdLetrasMi - 1; q++)
 {
-    Console.WriteLine(n);
+    int caractereAleatorio = aleatorio.Next(letrasMi.Length);
+    letrasMiSenha[q] = letrasMi[caractereAleatorio];
 }
-
-for (int q = 0; q <= qtdLetras - 1; q++)
+for (int q = 0; q <= qtdLetrasMa - 1; q++)
 {
-    int numeroAleatorio = aleatorio.Next(letras.Length - 1);
-    letrasSenha[q] = letras[numeroAleatorio];
+    int caractereAleatorio = aleatorio.Next(letrasMa.Length);
+    letrasMaSenha[q] = letrasMa[caractereAleatorio];
 }
-foreach (char l in letrasSenha)
-{
-    Console.WriteLine(l);
-}
-
 for (int q = 0; q <= qtdEspeciais - 1; q++)
 {
-    int numeroAleatorio = aleatorio.Next(especiais.Length - 1);
-    especiaisSenha[q] = especiais[numeroAleatorio];
+    int caractereAleatorio = aleatorio.Next(especiais.Length);
+    especiaisSenha[q] = especiais[caractereAleatorio];
 }
-foreach (char e in especiaisSenha)
+
+char[] senha = numerosSenha.Concat(letrasMiSenha).Concat(letrasMaSenha).Concat(especiaisSenha).ToArray();
+
+Console.Write("Sua senha: ");
+
+for (int q = 0; q <= qtdCaracteres - 1; q++)
 {
-    Console.WriteLine(e);
+    int caractereAleatorio = aleatorio.Next(senha.Length);
+    Console.Write(senha[caractereAleatorio]);
 }
