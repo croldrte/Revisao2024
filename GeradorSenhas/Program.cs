ba4f93aea8501f00a171e707fc6869600be92de0
Carol Duarte <carolduarte2000@gmail.com>
2025-01-20 18:10:52 -0300

    Pequena correção na lógica for

--- GeradorSenhas/Program.cs
+++ GeradorSenhas/Program.cs
@@ -1,4 +1,4 @@
-﻿char[] numeros = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+﻿char[] numeros = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 char[] letras = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 char[] especiais = { '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '{', '}', '[', ']', '/', '?', '"', ':', ';' };
 
@@ -67,14 +67,11 @@ else
     qtdEspeciais = 0;
 }
 
-for (int n = 0; n <= qtdNumeros; n++)
-{  
-    for (int s = 0; s <= qtdNumeros; s++)
-    { 
-        int numeroAleatorio = aleatorio.Next(numeros.Length);
-        numerosSenha[s] = letras[numeroAleatorio];       
-    }
-}   
+for (int s = 0; s <= qtdNumeros - 1; s++)
+{
+    int numeroAleatorio = aleatorio.Next(numeros.Length);
+    numerosSenha[s] = letras[numeroAleatorio];
+}
 
 foreach (char n in numerosSenha)
 {
